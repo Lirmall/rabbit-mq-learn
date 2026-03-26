e44c393b0ea71984ffcf07828c7e7dad0139c55d
Andrey Klokov <andreyk-orlangyr@mail.ru>
2026-03-26 15:17:44 +0300

    Init commit

---
+++ src/main/java/ru/klokov/rabbitmqlearn/RabbitMqLearnApplication.java
@@ -0,0 +1,13 @@
+package ru.klokov.rabbitmqlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMqLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/klokov/rabbitmqlearn/RabbitMqLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.klokov.rabbitmqlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
